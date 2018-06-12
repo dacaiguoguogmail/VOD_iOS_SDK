5392821d7ae28e91ec783b9bb173d122a6b1cf25
Han Ruisong <550399108@qq.com>
2018-06-12 19:27:12 +0800

    删除旧版本

--- Demo/Demo/DWUploadViewController.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "DWSDK.h"
-
-@interface DWUploadViewController : UIViewController <UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
-
-@end
