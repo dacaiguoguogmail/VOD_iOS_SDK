fd0367d04ae7313030dbcef0e14926652b07d408
Han Ruisong <550399108@qq.com>
2018-11-23 18:09:11 +0800

    升级到3.3.0
    1. 视频问答新增单选多选类型
    2. 新增网络检测功能
    3. 原片实时播放流程处理
    4. 修复了demo中的一些bug

---
+++ Demo/Demo/DWCustomPlayerViewController.h
@@ -0,0 +1,18 @@
+#import <UIKit/UIKit.h>
+#import "DWSDK.h"
+
+@interface DWCustomPlayerViewController : UIViewController
+
+@property (copy, nonatomic)NSString *videoId;
+@property (copy, nonatomic)NSString *localPath;
+@property (assign, nonatomic)BOOL playMode;
+@property (strong, nonatomic)NSArray *videos;
+@property (assign, nonatomic)NSInteger indexpath;
+
+@property (nonatomic,copy)NSArray *localArray;//本地播放数组
+
+@property (nonatomic,copy)NSString *verificationCode;
+
++ (instancetype)sharedInstance;
+
+@end
